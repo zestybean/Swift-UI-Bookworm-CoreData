931b7006dd3a97c25e8dbdfa04e4d3efa5c514d0
Beandip <brando.lugo@gmail.com>
2021-02-08 17:28:04 -0600

    Initial Commit

---
+++ Bookworm-CoreData/Bookworm_CoreDataApp.swift
@@ -0,0 +1,20 @@
+//
+//  Bookworm_CoreDataApp.swift
+//  Bookworm-CoreData
+//
+//  Created by Brando Lugo on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Bookworm_CoreDataApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
